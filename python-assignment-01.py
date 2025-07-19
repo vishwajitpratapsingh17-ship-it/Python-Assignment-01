435b68d7b33db7bd4a8ee0832eca03b8a1ea03cf
vishwajitpratapsingh17-ship-it <vishwajitpratapsingh17@gmail.com>
2025-07-19 21:21:44 +0530

    Add files via upload

---
+++ python-assignment-01.py
@@ -0,0 +1,15 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print(a + b)
+print()
+print(a - b)
+print()
+print(a * b)
+print()
+print(a / b)
+
+
+a =str(input("Enter first name: "))
+b =str(input("Enter last name: "))
+full_name = a +" " + b
+print("Hello!, ", full_name + "! Welcome to TDH." )
